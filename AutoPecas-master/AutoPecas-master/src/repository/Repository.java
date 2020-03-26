bc3d144872a1deaf97323b01c3a55a0116d2de7d
Thiago <thiagoaraujo.tec@gmail.com>
2020-03-26 11:58:07 -0300

    .

---
+++ AutoPecas-master/AutoPecas-master/src/repository/Repository.java
@@ -0,0 +1,76 @@
+package repository;
+
+import javax.persistence.EntityManager;
+
+import application.RepositoryException;
+import factory.JPAFactory;
+import model.DefaultEntity;
+
+public class Repository<T extends DefaultEntity<T>> {
+	private EntityManager entityManager;
+
+	public Repository() {
+		entityManager = JPAFactory.getEntityManager();
+	}
+
+	public Repository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	public void beginTransaction() throws RepositoryException {
+		try {
+			getEntityManager().getTransaction().begin();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RepositoryException("Problema ao iniciar uma transacao");
+		}
+	}
+
+	public void commitTransaction() throws RepositoryException {
+		try {
+			getEntityManager().getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RepositoryException("Problema ao comitar uma transacao");
+		}
+	}
+
+	public void rollBackTransaction() {
+		try {
+			getEntityManager().getTransaction().rollback();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void salvar(T entity) throws RepositoryException {
+		try {
+			getEntityManager().merge(entity);
+		} catch (Exception e) {
+			System.out.println("Erro no reposit�rio ao executar o metodo merge.");
+			e.printStackTrace();
+			throw new RepositoryException("Erro ao salvar");
+		}
+
+	}
+
+	public void excluir(T entity) throws RepositoryException {
+		try {
+			T obj = getEntityManager().merge(entity);
+			getEntityManager().remove(obj);
+		} catch (Exception e) {
+			System.out.println("Erro no reposit�rio ao executar o metodo merge.");
+			e.printStackTrace();
+			throw new RepositoryException("Erro ao salvar");
+		}
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+}
