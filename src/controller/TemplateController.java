25fe496757de27675b30bc40fe6cf20250544062
cfgaspar10 <carlesandrogaspar@gmail.com>
2020-03-26 13:39:54 -0300

    Nova Sincronização

--- src/controller/TemplateController.java
+++ src/controller/TemplateController.java
@@ -1,24 +1,23 @@
 package controller;
+
 import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import application.Session;
 import model.Usuario;
+
 @Named
 @ViewScoped
-public class TemplateController implements Serializable{
-	
-	/**
-	 * 
-	 */
+public class TemplateController implements Serializable {
+
 	private static final long serialVersionUID = 206276099339678780L;
 	private Usuario usuarioLogado;
 
 	public Usuario getUsuarioLogado() {
 		if (usuarioLogado == null) {
 			// buscando o usuario da sessao
-			usuarioLogado = (Usuario)Session.getInstance().getAttribute("usuarioLogado");
+			usuarioLogado = (Usuario) Session.getInstance().getAttribute("usuarioLogado");
 			if (usuarioLogado == null)
 				usuarioLogado = new Usuario();
 		}
@@ -28,10 +27,10 @@ public class TemplateController implements Serializable{
 	public void setUsuarioLogado(Usuario usuarioLogado) {
 		this.usuarioLogado = usuarioLogado;
 	}
-	
+
 	public String encerrarSessao() {
 		Session.getInstance().invalidateSession();
 		return "login.xhtml?faces-redirect=true";
 	}
-	
+
 }
