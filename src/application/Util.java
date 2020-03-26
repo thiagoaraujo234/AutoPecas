1e5f4c98bd90000e5ff39a3e8814d61735ff3b3f
cfgaspar10 <carlesandrogaspar@gmail.com>
2020-03-25 22:37:24 -0300

    Atualização Template

---
+++ src/application/Util.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class Util {
+
+	
+	public static String hashSHA256(String valor) {
+		return DigestUtils.sha256Hex(valor);
+	}
+	
+	public static void redirect(String url) {
+		try {
+			FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+		} catch (IOException e) {
+			addMessageError("Erro ao redirecionar a p�gina.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void addMessageInfo(String message) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, message, null));
+	}
+
+	public static void addMessageWarn(String message) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, message, null));
+	}
+
+	public static void addMessageError(String message) {
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, message, null));
+	}
+}
---
+++ src/controller/LoginController.java
@@ -0,0 +1,64 @@
+package controller;
+
+import factory.JPAFactory;
+import model.Usuario;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import application.Util;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class LoginController extends Controller<Usuario> implements Serializable {
+
+   
+	private static final long serialVersionUID = 8843973377372239240L;
+	private String filtro;
+
+
+    public void entrar() {
+        String senha = Util.hashSHA256(getEntity().getSenha());
+
+        System.out.println(senha);
+
+        EntityManager em = JPAFactory.getEntityManager();
+        Query query = em.createQuery("Select a " + "From Usuario a " + "Where a.cpf = :cpf AND a.senha = :senha");
+        query.setParameter("senha", senha);
+        query.setParameter("cpf", getEntity().getCpf());
+        try {
+            entity = (Usuario) query.getSingleResult();
+
+        } catch (NoResultException e) {
+            entity = null;
+        }
+
+        if (entity != null)
+            Util.redirect("hello.xhtml");
+        else
+            Util.addMessageError("Erro");
+    }
+
+    public String getFiltro() {
+        return filtro;
+    }
+
+    public void setFiltro(String filtro) {
+        this.filtro = filtro;
+    }
+
+    @Override
+    public Usuario getEntity() {
+        if (entity == null)
+            entity = new Usuario();
+        return entity;
+    }
+
+}
